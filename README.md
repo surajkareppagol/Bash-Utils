6228a06048a846333f1c3c5821ace97271747fec
surajkareppagol <surajkareppagol.dev@gmail.com>
2023-10-18 12:25:19 +0530

    📷️ Add create_gif.sh to convert mp4 to gif

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bash Utils
+
+## Create GIF'f From Videos
+
+The `create_gif.sh` script takes the name of the file (without extension) as parameter and generates `output.gif`.
+
+```bash
+chmod +x ./create_gif.sh
+```
+
+```bash
+./create_gif.sh input.mp4
+```
+
+Make sure `ffmpeg` is installed.
---
+++ create_gif.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change the scale parameter to change the GIF size
+ffmpeg -i $1.mp4 -vf "fps=10,scale=480:-1:flags=lanczos" -c:v pam -f image2pipe - | convert -delay 10 - -loop 0 -layers optimize $1.gif > ~/.bash_util.txt
+
+# Clear the terminal
+clear
+
+# Print log message
+echo "Log has been saved at ~/.bash_util.txt"
